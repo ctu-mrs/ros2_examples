a9e96720b0b891d7baed17f85b71b3cdfe456ed7
afzal <afzalhmd14@gmail.com>
2024-10-22 17:10:19 +0200

    update: switched to a simpler sync/async mode but the sync mode waits indefintely

--- src/service_client_example.cpp
+++ src/service_client_example.cpp
@@ -22,13 +22,12 @@ namespace ros2_examples
 
     // | ------------------------- timers ------------------------- |
 
-    rclcpp::TimerBase::SharedPtr timer_future_;
-    rclcpp::TimerBase::SharedPtr timer_cb_;
+    rclcpp::TimerBase::SharedPtr timer_async_;
+    rclcpp::TimerBase::SharedPtr timer_sync_;
 
-    rclcpp::Client<std_srvs::srv::SetBool>::SharedFuture future_setbool_;
-    void callback_timer_future();
+    void callback_timer_async();
 
-    void callback_timer_cb();
+    void callback_timer_sync();
   };
 
   //}
@@ -51,8 +50,8 @@ namespace ros2_examples
     // Examples for each case follow.
 
     // both the timers are going to be run inside the same thread as this example will be run inside a Single Threaded Executor
-    timer_future_ = create_wall_timer(std::chrono::duration<double>(1.0 / 10.0), std::bind(&ServiceClientExample::callback_timer_future, this));
-    timer_cb_ = create_wall_timer(std::chrono::duration<double>(1.0 / 10.0), std::bind(&ServiceClientExample::callback_timer_cb, this));
+    timer_async_ = create_wall_timer(std::chrono::duration<double>(2.0 / 1.0), std::bind(&ServiceClientExample::callback_timer_async, this));
+    /* timer_sync_ = create_wall_timer(std::chrono::duration<double>(2.0 / 1.0), std::bind(&ServiceClientExample::callback_timer_sync, this)); */
 
     // | --------------------- finish the init -------------------- |
 
@@ -66,88 +65,105 @@ namespace ros2_examples
   /* timer_future() //{ */
 
   // a timer callback should never block for too long, so all checks should return after a while
-  void ServiceClientExample::callback_timer_future()
+  void ServiceClientExample::callback_timer_sync()
   {
     // if the future is already waiting for a result, check if we got one
-    if (future_setbool_.valid())
-    {
-        const std::future_status status = future_setbool_.wait_for(std::chrono::milliseconds(100));;
-        if (status == std::future_status::ready)
-        {
-          // future already got a result, check it and report to user
-          const auto result = future_setbool_.get();
-          // reset the future
-          future_setbool_ = {};
-          if (result->success)
-          {
-            RCLCPP_INFO_STREAM(get_logger(), "future finished, service call successful with message: " << result->message);
-          }
-          else
-          {
-            RCLCPP_INFO_STREAM(get_logger(), "future finished, service call reports failure: " << result->message);
-          }
-        }
-        else
-        {
-          // future isn't ready yet
-          RCLCPP_INFO_STREAM(get_logger(), "service future not ready yet, waiting again...");
-        }
-    }
-    // if the future is not waiting for a result, make a new service call
-    else
-    {
-      // you should not send a service request unless the service is ready, otherwise the future will never be ready
+    /* if (future_setbool_.valid()) */
+    /* { */
       if (service_client_->service_is_ready())
       {
         auto request = std::make_shared<std_srvs::srv::SetBool::Request>();
         request->data = true;
-        future_setbool_ = service_client_->async_send_request(request).future.share();
-        RCLCPP_INFO_STREAM(get_logger(), "called service using future");
+
+        const auto future_setbool = service_client_->async_send_request(request).future.share();
+        RCLCPP_INFO_STREAM(get_logger(), "Service called in sync mode. Timer will wait for the response.");
+
+        if (future_setbool.valid() == false)
+        {
+          RCLCPP_WARN_STREAM(get_logger(), "Future object was in undefined state.");
+          return;
+        }
+
+        /* const auto status = future_setbool.wait_for(std::chrono::seconds(4)); */
+        /* if (status == std::future_status::ready) */
+        /* { */
+          // future already got a result, check it and report to user
+          const auto result = future_setbool.get();
+          if (result->success)
+          {
+            RCLCPP_INFO_STREAM(get_logger(), "Service call successful with message: " << result->message);
+          }
+          else
+          {
+            RCLCPP_INFO_STREAM(get_logger(), "Service call reports failure: " << result->message);
+          }
+        /* } */
+        /* else */
+        /* { */
+        /*   RCLCPP_WARN_STREAM(get_logger(), "Timeout reached while waiting for service response!"); */
+        /* } */
       }
       else
       {
-        RCLCPP_WARN_STREAM(get_logger(), "not calling service using future, service not ready!");
+        RCLCPP_WARN_STREAM(get_logger(), "Can not call service in sync mode. Service not ready!");
       }
-    }
+
+    /* } */
+    // if the future is not waiting for a result, make a new service call
+    /* else */
+    /* { */
+    /*   // you should not send a service request unless the service is ready, otherwise the future will never be ready */
+    /*   if (service_client_->service_is_ready()) */
+    /*   { */
+    /*     auto request = std::make_shared<std_srvs::srv::SetBool::Request>(); */
+    /*     request->data = true; */
+    /*     future_setbool_ = service_client_->async_send_request(request).future.share(); */
+    /*     RCLCPP_INFO_STREAM(get_logger(), "called service using future"); */
+    /*   } */
+    /*   else */
+    /*   { */
+    /*     RCLCPP_WARN_STREAM(get_logger(), "not calling service using future, service not ready!"); */
+    /*   } */
+    /* } */
   }
 
   //}
 
   // | ------------------------ routines ------------------------ |
 
-  // The callback function for service call response.
-  // This function will be executed once the other side decides to respond to the service call.
-  void callback_future_setbool(const rclcpp::Client<std_srvs::srv::SetBool>::SharedFuture fut)
-  {
-    // the future must be ready now that the callback was called
-    const auto result = fut.get();
-    if (result->success)
-    {
-      std::cout << "callback called, service call successful with message: " << result->message << std::endl;
-      /* RCLCPP_INFO_STREAM(get_logger(), "[" << get_name() << "]: callback called, service call successful with message: " << result->message); */
-    }
-    else
-    {
-      std::cout << "callback called, service call reports failure: " << result->message << std::endl;
-      /* RCLCPP_INFO_STREAM(get_logger(), "[" << get_name() << "]: callback called, service call reports failure: " << result->message); */
-    }
-  }
+
 
   /* timer_callback() //{ */
 
-  void ServiceClientExample::callback_timer_cb()
+  void ServiceClientExample::callback_timer_async()
   {
     // it's good to first check if the service server is even ready to be called
     if (service_client_->service_is_ready())
     {
       auto request = std::make_shared<std_srvs::srv::SetBool::Request>();
       request->data = true;
-      future_setbool_ = service_client_->async_send_request(request, &callback_future_setbool).future;
-      RCLCPP_INFO_STREAM(get_logger(), "[" << get_name() << "]: called service using callback");
+      service_client_->async_send_request(request, [&](const rclcpp::Client<std_srvs::srv::SetBool>::SharedFuture fut)
+        // The callback function for service call response.
+        // This function will be executed once the other side decides to respond to the service call.
+        {
+          // the future must be ready now that the callback was called
+          const auto result = fut.get();
+          if (result->success)
+          {
+            RCLCPP_INFO_STREAM(get_logger(), "Async callback called, service call successful with message: " << result->message);
+          }
+          else
+          {
+            RCLCPP_INFO_STREAM(get_logger(), "Async callback called, service call reports failure: " << result->message);
+          }
+        }
+        );
+
+      RCLCPP_INFO_STREAM(get_logger(), "Service called in async mode with a callback. Timer will not wait.");
     }
     else
     {
-      RCLCPP_INFO_STREAM(get_logger(), "[" << get_name() << "]: not calling service using callback, service not ready!");
+      RCLCPP_INFO_STREAM(get_logger(), "Can not call service in async mode. Service not ready!");
     }
   }
 
