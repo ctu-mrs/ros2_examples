1352c20a983d44aa8b3331a6a7c1c53c594d3541
Tomas Baca <Tomas Baca>
2025-11-21 06:22:07 +0100

    removed unused examples

--- extended_examples/waypoint_flier/tmux/arm_offboard.sh
+++
@@ -1,9 +0,0 @@
-echo "arming"
-
-ros2 service call /uav1/hw_api/arming std_srvs/srv/SetBool '{"data": true}' 
-
-sleep 1.0
-
-echo "toggling offboard"
-
-ros2 service call /uav1/hw_api/offboard std_srvs/srv/Trigger '{}' 
